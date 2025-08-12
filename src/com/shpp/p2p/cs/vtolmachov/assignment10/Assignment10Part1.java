c98825b395f71fac2fc3d05701c1dbcfe82d37e6
TeHbker <92017355+TeHbker@users.noreply.github.com>
2025-08-13 00:07:57 +0300

    Update Assignment10Part1.java

--- src/com/shpp/p2p/cs/vtolmachov/assignment10/Assignment10Part1.java
+++ src/com/shpp/p2p/cs/vtolmachov/assignment10/Assignment10Part1.java
@@ -2,12 +2,18 @@ package com.shpp.p2p.cs.vtolmachov.assignment10;
 
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Map;
 import java.util.Stack;
 
 public class Assignment10Part1 {
 
-    private static final HashMap<String, Integer> operatorsPriority = new HashMap<>();
+    private static final Map<String, Integer> OPERATORS_PRIORITY = Map.of("+", 1,
+            "-", 1,
+            "*", 2,
+            "/", 2,
+            "^", 3,
+            "(", 0,
+            ")", 0);
 
     public static void main(String[] args) {
         if (args != null && args.length > 0) {
@@ -18,19 +24,19 @@ public class Assignment10Part1 {
 
     /**
      * Main method that's calculates formula that we get from console
-     * @param   args incoming parameters with "formula" "arg1=number" "arg2=number" etc
-     * @return  answer on given formula
+     *
+     * @param args incoming parameters with "formula" "arg1=number" "arg2=number" etc
+     * @return answer on given formula
      */
     private static double calculate(String[] args) {
         FormulaNormalizer n = new FormulaNormalizer(args);
         ArrayList<String> arrayOfSymbols = n.getFullFormula();
-        // System.out.println("Formula: " + arrayOfSymbols);
+        System.out.println("Formula: " + arrayOfSymbols);
         Stack<Double> stackOfNumbers = new Stack<>();
         Stack<String> stackOfOperators = new Stack<>();
 
-        createPriorityMapOfOperators();
         calculateBasedOnPriority(arrayOfSymbols, stackOfNumbers, stackOfOperators);
-        emptyLeftOperators(stackOfOperators, stackOfNumbers);
+
 
         return stackOfNumbers.pop();
     }
@@ -40,17 +46,16 @@ public class Assignment10Part1 {
             if (!symbol.matches("[+\\-*/^()]")) {
                 stackOfNumbers.push(Double.parseDouble(symbol));
             } else {
-                if (stackOfOperators.isEmpty() || symbol.equals("(") || symbol.equals(")")) {
+                if (stackOfOperators.isEmpty() || symbol.equals("(")) {
                     stackOfOperators.push(symbol);
-                } else {
-                    /*
-                    TODO: Виправити помилку -(12 + 12+ 12). Краще прописати умову типу якщо одне число і один опер - множити на 1
-                     */
-                    if (stackOfOperators.peek().equals(")")) {
-                        specialConditionsForBrackets(stackOfNumbers, stackOfOperators);
-                    }
+                } else if (symbol.equals(")")) {
 
-                    if (operatorsPriority.get(symbol) <= operatorsPriority.get(stackOfOperators.peek())) {
+                    emptyLeftOperators(stackOfOperators, stackOfNumbers);
+                    stackOfOperators.pop(); // This is for the last symbol that appears and its 100% is "("
+
+                } else {
+
+                    if (OPERATORS_PRIORITY.get(symbol) <= OPERATORS_PRIORITY.get(stackOfOperators.peek())) {
                         stackOfNumbers.push(operateTwoNumbers(stackOfOperators.pop(), stackOfNumbers.pop(), stackOfNumbers.pop()));
                     }
                     stackOfOperators.push(symbol);
@@ -59,16 +64,20 @@ public class Assignment10Part1 {
 
             }
         }
+        emptyLeftOperators(stackOfOperators, stackOfNumbers);
     }
 
     private static void emptyLeftOperators(Stack<String> stackOfOperators, Stack<Double> stackOfNumbers) {
-            while (!stackOfOperators.isEmpty()) {
-                stackOfNumbers.push(operateTwoNumbers(stackOfOperators.pop(), stackOfNumbers.pop(), stackOfNumbers.pop()));
+        while (!stackOfOperators.isEmpty()) {
+            if(stackOfOperators.peek().equals("(")){
+                break;
             }
+            stackOfNumbers.push(operateTwoNumbers(stackOfOperators.pop(), stackOfNumbers.pop(), stackOfNumbers.pop()));
+        }
     }
 
     private static double operateTwoNumbers(String symbol, double num2, double num1) {
-        double answ = switch (symbol) {
+        return switch (symbol) {
             case "+" -> num1 + num2;
             case "-" -> num1 - num2;
             case "*" -> num1 * num2;
@@ -76,19 +85,7 @@ public class Assignment10Part1 {
             case "^" -> Math.pow(num1, num2);
             default -> throw new IllegalStateException("Unexpected value: " + symbol);
         };
-        return answ;
-    }
-
-    private static void createPriorityMapOfOperators() {
-        operatorsPriority.put("+", 1);
-        operatorsPriority.put("-", 1);
-        operatorsPriority.put("*", 2);
-        operatorsPriority.put("/", 2);
-        operatorsPriority.put("^", 3);
-        operatorsPriority.put("(", 0);
-        operatorsPriority.put(")", 0);
     }
 
 }
-
 
